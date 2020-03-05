688665866d3e57e44ac7d96f461d1a1273cd35dc
KristjanBajuk <kristjan.bajuk@ineor.si>
2020-03-05 15:09:49 +0100

    add "convert" socket listener

--- index.js
+++ index.js
@@ -45,12 +45,6 @@ const downloadVideo = (videoUrl, socket) => {
             const downloadedMinutes = (Date.now() - starttime) / 1000 / 60;
             const progress = percent.toFixed(0);
             socket.emit("progress", {progress: progress});
-            // readline.cursorTo(process.stdout, 0);
-            // process.stdout.write(`${(percent * 100).toFixed(2)}% downloaded `);
-            // process.stdout.write(`(${(downloaded / 1024 / 1024).toFixed(2)}MB of ${(total / 1024 / 1024).toFixed(2)}MB)\n`);
-            // process.stdout.write(`running for: ${downloadedMinutes.toFixed(2)}minutes`);
-            // process.stdout.write(`, estimated time left: ${(downloadedMinutes / percent - downloadedMinutes).toFixed(2)}minutes `);
-            // readline.moveCursor(process.stdout, 0, -1);
         });
 
         video.on('end', () => {
@@ -71,6 +65,10 @@ io.on('connection', (socket) => {
         } else {
             socket.emit("validateUrl", {valid: false, url: null});
         }
+    });
+
+    socket.on('convert', (data) => {
+        downloadVideo(data.url, socket);
     });
 });
 
